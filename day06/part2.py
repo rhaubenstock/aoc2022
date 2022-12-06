7606978b41240c38186893814a4adddc7bf78431
Rhaubenstock <13337987+rhaubenstock@users.noreply.github.com>
2022-12-05 22:41:54 -0800

    Solved day06

---
+++ day06/part2.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    arr = [0]*26
+    k = 14
+    for i in range(k-1):
+        arr[ord(s[i])-97] += 1
+    countDupes = k-1 - len(set(s[:k-1]))
+
+    for i, c in enumerate(s[k-1:]):
+        if arr[ord(s[i + k-1])-97] > 0:
+            countDupes += 1
+        arr[ord(s[i + k-1])-97] += 1
+        if countDupes == 0:
+            return i + k
+        if arr[ord(s[i])-97] > 1:
+            countDupes -= 1
+        arr[ord(s[i])-97] -= 1
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
