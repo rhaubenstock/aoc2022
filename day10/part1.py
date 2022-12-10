e2d551c17f192f174b2c38a698901edece2acc2c
Rhaubenstock <13337987+rhaubenstock@users.noreply.github.com>
2022-12-09 22:07:25 -0800

    Solved day10

--- day10/part1.py
+++ day10/part1.py
@@ -3,11 +3,42 @@ from __future__ import annotations
 import argparse
 import os.path
 
-INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input2.txt')
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 
 def compute(s: str) -> int:
-    return
+    lines = s.split("\n")
+    signal = 1
+    scoreAcc = 0
+    n = len(lines)
+    cycle = 1
+    instr = 0
+    endCond = 21
+    sameInstr = False
+    addAmt = 0
+    while instr < n:
+        while cycle < endCond and instr < n:
+            # print("instr", instr)
+            # print("cycle", cycle)
+            cycle += 1
+            if sameInstr:
+                sameInstr = False
+                instr += 1
+                continue
+            signal += addAmt
+            addAmt = 0
+            lineArr = lines[instr].split()
+            if lineArr[0] != "addx":
+                instr += 1
+                continue
+            sameInstr = True
+            addAmt = int(lineArr[1])
+
+        if cycle == endCond:
+            scoreAcc += signal * (cycle-1)
+        endCond += 40
+
+    return scoreAcc
 
 
 def main() -> int:
--- day10/part2.py
+++ day10/part2.py
@@ -3,11 +3,40 @@ from __future__ import annotations
 import argparse
 import os.path
 
-INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input2.txt')
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 
 def compute(s: str) -> int:
-    return
+    screenArr = [['.']*40 for _ in range(6)]
+    lines = s.split("\n")
+    signal = 1
+    n = len(lines)
+    instr = 0
+    endCond = 21
+    sameInstr = False
+    addAmt = 0
+
+    for r in range(6):
+        for c in range(40):
+            if sameInstr:
+                sameInstr = False
+                instr += 1
+                if abs(c - signal) < 2:
+                    screenArr[r][c] = '#'
+                continue
+            signal += addAmt
+            if abs(c - signal) < 2:
+                screenArr[r][c] = '#'
+            addAmt = 0
+            lineArr = lines[instr].split()
+            if lineArr[0] != "addx":
+                instr += 1
+                continue
+            sameInstr = True
+            addAmt = int(lineArr[1])
+
+    for rowArr in screenArr:
+        print(''.join(rowArr))
 
 
 def main() -> int:
