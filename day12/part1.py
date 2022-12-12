1d761a6b14c1e8bb54699ed4595993880cebb16d
Rhaubenstock <13337987+rhaubenstock@users.noreply.github.com>
2022-12-11 21:51:11 -0800

    Solved day12

---
+++ day12/part1.py
@@ -0,0 +1,76 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    #BFS
+    #want shortest path from S to E
+    start = None
+
+    lines = s.split("\n")
+    flag = False
+    for r,line in enumerate(lines):
+        for c, char in enumerate(line):
+            if char == 'S':
+                start = (r,c,0,97)
+                flag = True
+                break
+        if flag:
+            break
+
+    rows = len(lines)
+    cols = len(lines[0])
+
+    q = [start]
+    visited = set([(r,c)])
+
+    def visitable(row, col, prev):
+        validCell = row >= 0 and row < rows and col >= 0 and col < cols
+        if not validCell or (row,col) in visited:
+            return False
+        charOrd = ord(lines[row][col])
+        if charOrd == 69:
+            charOrd = 122
+        return charOrd <= prev + 1
+
+    def tryVisit(r, c, d, p):
+        if visitable(r,c,p):
+            visited.add((r,c))
+            q.append((r, c, d+1, ord(lines[r][c])))
+            if lines[r][c] == 'E':
+                return True
+        return False
+
+    while q:
+        r,c,d,p = q.pop(0)       
+
+        if tryVisit(r-1,c,d,p):
+            return d + 1
+        if tryVisit(r+1,c,d,p):
+            return d + 1
+        if tryVisit(r,c-1,d,p):
+            return d + 1
+        if tryVisit(r,c+1,d,p):
+            return d + 1
+    
+    # print(visited)
+    return -1
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
